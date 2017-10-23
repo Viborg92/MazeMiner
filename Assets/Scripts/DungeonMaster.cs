dd6bbf34f42f4f37dc3021453694abe70c690579
Viborg <Viborg92@hotmail.com>
2017-10-23 09:40:57 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/DungeonMaster.cs
+++ Assets/Scripts/DungeonMaster.cs
@@ -65,3 +65,70 @@ public class DungeonMaster : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Dungeon master.
+/// Applies and uses all other functions to generate and setup the map.
+/// </summary>
+public class DungeonMaster : MonoBehaviour
+{
+    [SerializeField, Tooltip("The total amount of rooms being spawned")] int numOfRoomsToSpwan = 20;
+    [SerializeField, Tooltip("The amount of biggest rooms that should be kept")] int numOfRoomsToKeep = 10;
+    [SerializeField, Tooltip("The amount of connections pr. room")] int connectionPrRoom = 2;
+    List<Room> biggestRooms;
+    public bool isDoneMoving;
+
+
+    RoomFactory roomfactory;
+    NormalDistribution normaldistribution;
+    RoomFinder roomfinder;
+    RouteMaker routemaker;
+    // Use this for initialization
+    void Awake()
+    {
+        roomfactory = GetComponent<RoomFactory>();
+        normaldistribution = GetComponent<NormalDistribution>();
+        roomfinder = GetComponent<RoomFinder>();
+        routemaker = GetComponent<RouteMaker>();
+    }
+
+    public void Start()
+    {
+        isDoneMoving = false;
+        MineMaker();
+        biggestRooms = roomfinder.FindTheBiggest(numOfRoomsToKeep).ToList();
+    }
+
+    public void Update()
+    {
+        if (!isDoneMoving)
+        {
+            RoomMoveChecker();
+        }
+    }
+
+    void RoomMoveChecker()
+    {
+        if (roomfactory.rooms.All(obj => !obj.moving))
+        {
+            isDoneMoving = true;
+            roomfinder.FindClosestRoom(biggestRooms, connectionPrRoom);
+            routemaker.ChooseRoute(biggestRooms[Random.Range(0, biggestRooms.Count)]);
+        }
+    }
+
+
+    void MineMaker()
+    {
+        for (int i = 0; i < numOfRoomsToSpwan; i++)
+        {
+            roomfactory.Generate(normaldistribution.Gaussian(), normaldistribution.Gaussian(), Random.Range(-25, 25), Random.Range(-25, 25));
+        }
+    }
+}
+>>>>>>> Stashed changes
