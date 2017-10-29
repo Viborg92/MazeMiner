f3333f20e414773d3cadfa184542af5906d9f212
Unknown <Viborg92@hotmail.com>
2017-10-29 23:52:58 +0100

    Revert "Made sure that exit and player does not spawn in same room"
    This reverts commit d77b2c36e367b5119428fc624a7fa8b85fe11af6.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -17,22 +17,11 @@ public class PlayerManager : MonoBehaviour
         cameraBehavior = Camera.main.GetComponent<CameraBehavior>();
     }
 
-    public void spwanPlayer(List<Room> room)
+    public void spwanPlayer(Vector3 pos)
     {
-        Room spwaningRoom;
-
-        spwaningRoom = room[Random.Range(0, room.Count)];
-        if (spwaningRoom.tag != "exitRoom")
-        {
-            Vector3 pos = spwaningRoom.transform.position;
-            pos.z = -1;
-            GameObject newPlayer = Instantiate(player, pos, Quaternion.identity);
-            cameraBehavior.target = newPlayer;
-            newPlayer.SetActive(true);
-        }
-        else
-        {
-            spwaningRoom = room[Random.Range(0, room.Count)]; 
-        }
+        pos.z = -1;
+        GameObject newPlayer = Instantiate(player, pos, Quaternion.identity);
+        cameraBehavior.target = newPlayer;
+        newPlayer.SetActive(true);
     }
 }
