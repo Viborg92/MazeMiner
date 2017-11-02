32f1e4a769e2d8af29505c1ab3029d30a86c2a2a
Viborg <Viborg92@hotmail.com>
2017-11-02 09:40:35 +0100

    Started the dungeonJanitor

---
+++ Assets/Scripts/Dungeon generation/DungeonJanitor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonJanitor : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] parentObjs;
+
+    public void ClearDungeon()
+    {
+
+
+    }
+}
