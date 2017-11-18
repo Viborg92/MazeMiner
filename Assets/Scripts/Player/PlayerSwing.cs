88571bd70ee0f1a9d8d9db39eb08b1dc9e89451a
Viborg <Viborg92@hotmail.com>
2017-11-19 00:33:48 +0100

    tried to do some axe stuff

--- Assets/Scripts/Player/PlayerSwing.cs
+++ Assets/Scripts/Player/PlayerSwing.cs
@@ -6,11 +6,33 @@ public class PlayerSwing : MonoBehaviour
 {
     [SerializeField]
     private GameObject Arm;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    Transform pointA, pointB;
+    bool swingLeft = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Attack();
+        }
+    }
 
     void Attack()
     {
-        Arm.transform.Rotate(Vector3.right * Time.deltaTime);
+       
+        if (swingLeft)
+        {
+            Arm.transform.LookAt(pointA.transform.position, worldUp: transform.forward);
+            swingLeft = true;
+        }
+        else
+        {
+            Arm.transform.LookAt(pointB.transform.position, worldUp: transform.forward);
+            swingLeft = false;
+        }
     }
-
 
 }
