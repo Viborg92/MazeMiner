8d021d864a6a00f24316cda90499eddc40fe030e
Viborg <Viborg92@hotmail.com>
2017-12-02 17:54:03 +0100

    Change background color, and fixed attack ! :D

--- Assets/Scripts/Player/PlayerSwing.cs
+++ Assets/Scripts/Player/PlayerSwing.cs
@@ -5,21 +5,44 @@ using UnityEngine;
 public class PlayerSwing : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody2D rb2D;
+    private Transform axeTransform;
     [SerializeField]
     private float swingSpeed;
+    [SerializeField]
+    private float timeBeforeReturn = 1;
+    [SerializeField]
+    private float returnTime = 1;
 
-    void Update()
+    private float speed;
+
+    void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Attack();
+            StopAllCoroutines();
+            StartCoroutine(Swing());
         }
     }
 
-    void Attack()
+    IEnumerator Swing()
     {
-        rb2D.MoveRotation(rb2D.rotation + swingSpeed * Time.fixedDeltaTime);
-    }
+        speed += swingSpeed;
+        float localTime = 0;
 
+        while (localTime < timeBeforeReturn)
+        {
+            yield return null;
+            localTime += Time.deltaTime;
+            axeTransform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+        speed = 0;
+        localTime = 0;
+        float startRotation = axeTransform.transform.localEulerAngles.z;
+        while (localTime < returnTime)
+        {
+            yield return null;
+            localTime += Time.deltaTime;
+            axeTransform.localEulerAngles = Vector3.forward * Mathf.LerpAngle(startRotation, 0, localTime / returnTime);
+        }
+    }
 }
