b10c8c064f5ded23bdf7bb41f5076537e2512bbf
Unknown <Viborg92@hotmail.com>
2017-12-04 22:06:50 +0100

    Made the first draft of an enemy.

--- Assets/Scripts/Enemy/BaseEnemyBehaviorContext.cs
+++ Assets/Scripts/Enemy/BaseEnemyBehaviorContext.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Base enemy behavior context.
+/// The base class for all enemies, with the most general variables
+/// And functiones needed.
+/// </summary>
 public class BaseEnemyBehaviorContext : MonoBehaviour
 {
     [SerializeField, Tooltip("The health of the enemy")]
     protected float Health = 5;
     [SerializeField, Tooltip("The players position")]
     protected Transform playerPostion;
+    [SerializeField, Tooltip("The attack of the enemy")]
+    protected float attackPower = 2;
+    [SerializeField, Tooltip("Movement speed of a enemy")]
+    protected float movementSpeed = 5;
+
+    protected virtual void Awake()
+    {
+        playerPostion = GameObject.Find("Player").transform;
+    }
 }
---
+++ Assets/Scripts/Enemy/FlyingEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Flying enemy that flies towards the players
+/// And damages the player on collision.
+/// </summary>
+public class FlyingEnemy : BaseEnemyBehaviorContext
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    void Update()
+    {
+        movementTowardsPlayer();
+    }
+
+    void movementTowardsPlayer()
+    {
+        float step = movementSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, playerPostion.position, step);
+    }
+}
